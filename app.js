89dbc7eb9f4779f991e14c521de6e9208c27d9a4
Vikas Suresh Ashtekar <112936290+vikasashtekar@users.noreply.github.com>
2024-01-23 16:44:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+ let todo_list=[
+  { 
+    item:'buy milk',
+    duedate:'25/5/2001'
+  },
+  { 
+    item:'go to hell',
+    duedate:'20/8/2000'
+  }
+];
+ DisplayItems();
+
+ function addTodo(){
+  let inputElement=document.querySelector("#todo-input");
+  let dateElement=document.querySelector("#todo-date");
+  let todoItem=inputElement.value;
+  let tododate=dateElement.value;
+  todo_list.push({item:todoItem,
+    duedate:tododate});
+  inputElement.value='';
+  dateElement.value='';
+
+  DisplayItems();
+ }
+
+ function DisplayItems(){
+  let ContainerElement=document.querySelector(".todo-container");
+  let newHtml='';
+    for(let i=0;i<todo_list.length;i++){
+      let Item=todo_list[i].item;
+      let duedate=todo_list[i].duedate;
+      newHtml+=`
+        <span>${Item}</span>
+        <span>${duedate}</span>
+        <button class="btn_delete" onclick="todo_list.splice(${i},1);
+        DisplayItems();">Delete</button>
+      `;
+    }
+    ContainerElement.innerHTML=newHtml;
+  }
